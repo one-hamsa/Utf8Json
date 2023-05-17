bad3c85294c301570a00d449bd1379cce0747895
Omer Perry <thebne@gmail.com>
2023-05-17 12:47:48 +0300

    add code generator

---
+++ Utf8Json.UniversalCodeGenerator~/win-x64/Utf8JsonGenerated.cs
@@ -0,0 +1,174 @@
+﻿#pragma warning disable 618
+#pragma warning disable 612
+#pragma warning disable 414
+#pragma warning disable 168
+
+namespace Utf8Json.Resolvers
+{
+    using System;
+    using Utf8Json;
+
+    public class GeneratedResolver : global::Utf8Json.IJsonFormatterResolver
+    {
+        public static readonly global::Utf8Json.IJsonFormatterResolver Instance = new GeneratedResolver();
+
+        GeneratedResolver()
+        {
+
+        }
+
+        public global::Utf8Json.IJsonFormatter<T> GetFormatter<T>()
+        {
+            return FormatterCache<T>.formatter;
+        }
+
+        static class FormatterCache<T>
+        {
+            public static readonly global::Utf8Json.IJsonFormatter<T> formatter;
+
+            static FormatterCache()
+            {
+                var f = GeneratedResolverGetFormatterHelper.GetFormatter(typeof(T));
+                if (f != null)
+                {
+                    formatter = (global::Utf8Json.IJsonFormatter<T>)f;
+                }
+            }
+        }
+    }
+
+    internal static class GeneratedResolverGetFormatterHelper
+    {
+        static readonly global::System.Collections.Generic.Dictionary<Type, int> lookup;
+
+        static GeneratedResolverGetFormatterHelper()
+        {
+            lookup = new global::System.Collections.Generic.Dictionary<Type, int>(1)
+            {
+                {typeof(global::TestZLogger.UserRegisteredLog), 0 },
+            };
+        }
+
+        internal static object GetFormatter(Type t)
+        {
+            int key;
+            if (!lookup.TryGetValue(t, out key)) return null;
+
+            switch (key)
+            {
+                case 0: return new Utf8Json.Formatters.TestZLogger_UserRegisteredLogFormatter();
+                default: return null;
+            }
+        }
+    }
+}
+
+#pragma warning disable 168
+#pragma warning restore 414
+#pragma warning restore 618
+#pragma warning restore 612
+
+#pragma warning disable 618
+#pragma warning disable 612
+#pragma warning disable 414
+#pragma warning disable 219
+#pragma warning disable 168
+
+namespace Utf8Json.Formatters
+{
+    using System;
+    using Utf8Json;
+
+
+    public sealed class TestZLogger_UserRegisteredLogFormatter : global::Utf8Json.IJsonFormatter<global::TestZLogger.UserRegisteredLog>
+    {
+        readonly global::Utf8Json.Internal.AutomataDictionary ____keyMapping;
+        readonly byte[][] ____stringByteKeys;
+
+        public TestZLogger_UserRegisteredLogFormatter()
+        {
+            this.____keyMapping = new global::Utf8Json.Internal.AutomataDictionary()
+            {
+                { JsonWriter.GetEncodedPropertyNameWithoutQuotation("Id"), 0},
+                { JsonWriter.GetEncodedPropertyNameWithoutQuotation("Name"), 1},
+            };
+
+            this.____stringByteKeys = new byte[][]
+            {
+                JsonWriter.GetEncodedPropertyNameWithBeginObject("Id"),
+                JsonWriter.GetEncodedPropertyNameWithPrefixValueSeparator("Name"),
+                
+            };
+        }
+
+        public void Serialize(ref JsonWriter writer, global::TestZLogger.UserRegisteredLog value, global::Utf8Json.IJsonFormatterResolver formatterResolver)
+        {
+            
+
+            writer.WriteRaw(this.____stringByteKeys[0]);
+            writer.WriteInt32(value.Id);
+            writer.WriteRaw(this.____stringByteKeys[1]);
+            writer.WriteString(value.Name);
+            
+            writer.WriteEndObject();
+        }
+
+        public global::TestZLogger.UserRegisteredLog Deserialize(ref JsonReader reader, global::Utf8Json.IJsonFormatterResolver formatterResolver)
+        {
+            if (reader.ReadIsNull())
+            {
+                throw new InvalidOperationException("typecode is null, struct not supported");
+            }
+            
+
+            var __Id__ = default(int);
+            var __Id__b__ = false;
+            var __Name__ = default(string);
+            var __Name__b__ = false;
+
+            var ____count = 0;
+            reader.ReadIsBeginObjectWithVerify();
+            while (!reader.ReadIsEndObjectWithSkipValueSeparator(ref ____count))
+            {
+                var stringKey = reader.ReadPropertyNameSegmentRaw();
+                int key;
+                if (!____keyMapping.TryGetValueSafe(stringKey, out key))
+                {
+                    reader.ReadNextBlock();
+                    goto NEXT_LOOP;
+                }
+
+                switch (key)
+                {
+                    case 0:
+                        __Id__ = reader.ReadInt32();
+                        __Id__b__ = true;
+                        break;
+                    case 1:
+                        __Name__ = reader.ReadString();
+                        __Name__b__ = true;
+                        break;
+                    default:
+                        reader.ReadNextBlock();
+                        break;
+                }
+
+                NEXT_LOOP:
+                continue;
+            }
+
+            var ____result = new global::TestZLogger.UserRegisteredLog();
+            if(__Id__b__) ____result.Id = __Id__;
+            if(__Name__b__) ____result.Name = __Name__;
+
+            return ____result;
+        }
+    }
+
+}
+
+#pragma warning disable 168
+#pragma warning restore 219
+#pragma warning restore 414
+#pragma warning restore 618
+#pragma warning restore 612
